4fac99d315d228bee6d184fa699ea5d0de15b255
0xEAB <desisma@heidel.beer>
2017-07-18 20:06:44 +0200

    fixes, comments
     - added 'removeNth'
     - line endings: LF

--- source/algorithm/mutation.d
+++ source/algorithm/mutation.d
@@ -8,10 +8,20 @@ import std.traits : isArray;
 /++
     Creates a clone of a range
  +/
-@safe Range clone(T, Range)(Range r) if (isInputRange!Range)
+@safe Range clone(T, Range)(Range source) if (isInputRange!Range)
 {
-    auto output = new T[r.length];
-    output[0 .. r.length] = r;
+    auto output = new T[source.length];
+    output[0 .. source.length] = source;
+    return output;
+}
+/++ Creates a clone of an associative array +/
+@safe Te[Tidx] clone(Te, Tidx)(Te[Tidx] source)
+{
+    Te[Tidx] output;
+
+    foreach (idx, var; source)
+        output[idx] = var;
+
     return output;
 }
 
@@ -19,23 +29,54 @@ import std.traits : isArray;
 @safe unittest
 {
     int[] array = [10, 20, 30, 40, 10, 20, 30, 40];
+
+    // Clone is a clone of the array
     int[] clone = array.clone!int();
 
+    // Both have the same length.
     assert(clone.length == array.length);
-
+    // The clone contains the same elements as its source array
     foreach (idx, val; clone)
         assert(array[idx] == val);
+}
+/++ ditto +/
+@safe unittest
+{
+    int[string] associativeArray = ["one" : 122, "two" : 233, "three" : 344];
+
+    // Clone is a clone of the associative array
+    auto clone = associativeArray.clone();
+
+    // Both have the same length.
+    assert(clone.length == associativeArray.length);
+
+    // The clone contains the same elements as its source array
+    foreach (idx, val; clone)
+        assert(associativeArray[idx] == val);
 }
 
 /++
     Creates a type-casted clone of a range
  +/
-@safe T[] cloneCast(T, Range)(Range r) if (isInputRange!Range)
+@safe T[] cloneCast(T, Range)(Range source) if (isInputRange!Range)
 {
-    auto output = new T[r.length];
+    auto output = new T[source.length];
 
-    foreach (idx, var; r)
+    foreach (idx, var; source)
         output[idx] = cast(T) var;
+
+    return output;
+}
+
+/++
+    Creates a type-casted clone of an associative array
+ +/
+@safe Tnew[Tidx] cloneCast(Tnew, Told, Tidx)(Told[Tidx] source)
+{
+    Tnew[Tidx] output;
+
+    foreach (idx, var; source)
+        output[idx] = cast(Tnew) var;
 
     return output;
 }
@@ -43,20 +84,43 @@ import std.traits : isArray;
 /++ Examples: +/
 @safe unittest
 {
+    // The source array's element type is integer
     int[] array = [10, 20, 30, 40, 10, 20, 30, 40];
+
+    // The clone's elements are chars
     char[] clone = array.cloneCast!char();
 
+    // Both have the same length.
     assert(clone.length == array.length);
 
+    // The clone contains the same elements as its source array
     foreach (idx, val; clone)
         assert(array[idx] == val);
+}
+/++ ditto +/
+@safe unittest
+{
+    // The source array's element type is integer
+    int[string] associativeArray = ["one" : 122, "two" : 233, "three" : 344];
+
+    // The clone's elements are long integers
+    // The index's type remains unchanged
+    long[string] clone = associativeArray.cloneCast!long();
+
+    // Both have the same length.
+    assert(clone.length == associativeArray.length);
+
+    // The clone contains the same elements as its source array
+    foreach (idx, val; clone)
+        assert(associativeArray[idx] == val);
 }
 
 /++
     Copies the content of $(D source) into $(D target), performs type-casting
     and does not care about existing elements. They will get overriden.
 
-    See_Also: algorithm.mutation.copyOverrideInto
+    See_Also:
+        algorithm.mutation.copyOverrideInto
  +/
 @safe void copyCastOverrideInto(TargetType, SourceRange, TargetRange)(
         SourceRange source, TargetRange target)
@@ -76,18 +140,26 @@ import std.traits : isArray;
     int[] array0 = [10, 20, 30, 40];
     char[] array1 = ['a', 'b', 'c'];
 
+    // The elements of array1 are copied into array0
+    // Their type gets casted to int
     array1.copyCastOverrideInto!int(array0);
-
+    // As array1 has 3 elements,
+    // the first 3 elements of array0 get replaced by those from array1.
     assert(array0 == ['a', 'b', 'c', 40]);
 }
 
-alias copyInto = copy;
+/++
+    See_Also:
+        std.algorithm.copy
+ +/
+public alias copyInto = copy;
 
 /++
     Copies the content of $(D source) into $(D target)
     and does not care about existing elements. They will get overriden.
 
-    See_Also: std.algorithm.copy
+    See_Also:
+        std.algorithm.copy
  +/
 @safe void copyOverrideInto(SourceRange, TargetRange)(SourceRange source, TargetRange target)
         if (isArray!SourceRange && isArray!TargetRange
@@ -106,14 +178,74 @@ alias copyInto = copy;
     int[] array0 = [10, 20, 30, 40];
     int[] array1 = [50, 60, 70];
 
+    // The elements of array1 are copied into array0.
     array1.copyOverrideInto(array0);
-
+    // As array1 has 3 elements,
+    // the first 3 elements of array0 get replaced by those from array1.
     assert(array0 == [50, 60, 70, 40]);
+}
+
+/++
+    Removes the nth element from the given range
+    and replaces the refernce with the new shortened range.
+    
+    The state of the original range is undefined afterwards.
+ +/
+Range removeNth(Range)(ref Range r, ptrdiff_t n)
+        if (isInputRange!Range && !is(T == char))
+{
+    r = r.remove(n);
+    return r;
+}
+/++ ditto +/
+char[] removeNth(ref char[] r, ptrdiff_t n)
+{
+    // HACK: std.algorithm.remove() does *not* work for char[]
+    auto c = r.cloneCast!int();
+
+    c = c.remove(n);
+
+    r = c.cloneCast!char();
+    return r;
+}
+
+/++ Examples: +/
+@system unittest
+{
+    int[] array = [100, 200, 300, 400];
+    // Index:       0    1    2    3
+
+    // The 2nd element is removed
+    array.removeNth(2);
+
+    // The array's reference gets replaced
+    // by a new one that doesn't contain the removed element.
+    assert(!array.contains(300));
+    // Therefore, it is 1 element shorter.
+    assert(array == [100, 200, 400]);
+    assert(array.length == 3);
+}
+/++ ditto +/
+@system unittest
+{
+    char[] array = ['a', 'b', 'c', 'd'];
+    // Index:        0    1    2    3
+
+    // The 2nd element gets removed
+    array.removeNth(2);
+
+    // The array's reference gets replaced
+    // by a new one that doesn't contain the removed element.
+    assert(!array.contains('c'));
+    // Therefore, it is 1 element shorter.
+    assert(array == ['a', 'b', 'd']);
+    assert(array.length == 3);
 }
 
 /++
     Removes all occurences of an element from the given range
     and replaces the reference with the new shortened range.
+
     The state of the original range is undefined afterwards.
  +/
 Range removeAllOf(T, Range)(ref Range r, T element)
@@ -133,7 +265,6 @@ Range removeAllOf(T, Range)(ref Range r, T element)
 char[] removeAllOf(T)(ref char[] r, T element)
 {
     // HACK: std.algorithm.remove() does *not* work for char[]
-
     int[] c = cloneCast!int(r);
 
     c.removeAllOf(cast(int) element);
@@ -147,8 +278,13 @@ char[] removeAllOf(T)(ref char[] r, T element)
 {
     int[] array = [10, 20, 30, 40, 10, 20, 30, 40];
 
+    // All elements with the value 10 get removed
     array.removeAllOf(10);
+
+    // The array's reference gets replaced
+    // by a new one that doesn't contain any of the removed elements.
     assert(!array.contains(10));
+    // Therefore, it is shorter.
     assert(array.length == 6);
 }
 /++ ditto +/
@@ -156,8 +292,13 @@ char[] removeAllOf(T)(ref char[] r, T element)
 {
     char[] array = ['a', 'b', 'c', 'a', 'b', 'c'];
 
+    // All elements with the value 'b' get removed
     array.removeAllOf('b');
+
+    // The array's reference gets replaced
+    // by a new one that doesn't contain any of the removed elements.
     assert(!array.contains('b'));
+    // Therefore, it is shorter.
     assert(array.length == 4);
 }
 
@@ -196,8 +337,17 @@ char[] removeFirstOf(T)(ref char[] r, T element)
 {
     int[] array = [10, 20, 30, 40, 10, 20, 30, 40];
 
+    // The first element with the value 10 gets removed
     array.removeFirstOf(20);
+
+    // only the first one
     assert(array.contains(20));
+
+    // The array's reference got replaced
+    // by a new one that doesn't contain the removed element.
+    assert(array == [10, 30, 40, 10, 20, 30, 40]);
+
+    // Therefore, it is 1 element shorter.
     assert(array.length == 7);
 }
 /++ ditto +/
@@ -205,7 +355,16 @@ char[] removeFirstOf(T)(ref char[] r, T element)
 {
     char[] array = ['a', 'b', 'c', 'a', 'b', 'c'];
 
+    // The first element with the value 'b' gets removed
     array.removeFirstOf('b');
+
+    // only the first one
     assert(array.contains('b'));
+
+    // The array's reference got replaced
+    // by a new one that doesn't contain the removed element.
+    assert(array == ['a', 'c', 'a', 'b', 'c']);
+
+    // Therefore, it is 1 element shorter.
     assert(array.length == 5);
 }
